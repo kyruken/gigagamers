99c8a3153fe5070ac5963effe09bd1125a7f07e1
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-17 11:38:20 -0800

    refactor routes

--- routes/directory.js
+++ routes/directory.js
@@ -6,16 +6,19 @@ const userController = require('../controllers/userController');
 const messageController = require('../controllers/messageController');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render("homepage", {user: req.user});
+});
+
+router.get('/log-out', (req, res) => {
+    res.render("homepage", {message: "Logged out"});
 });
 
 //Creating a new user
-router.get('/user_form/create', userController.get_user_form);
-router.post('/user_form/create', userController.post_user_form);
+router.get('/sign-up', userController.get_user_form);
+router.post('/sign-up', userController.post_user_form);
 
 //Logging in with an existing user
-router.get('/user_login', userController.get_user_login);
-router.post('/user_login', userController.post_user_login);
+router.get('/log-in', userController.get_user_login);
 
 //Creating a new message
 router.get('/message_form', messageController.get_message_form);
