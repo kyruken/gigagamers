180fac1dfc1afe8a05249239311ebdc6632ff138
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-16 18:33:09 -0800

    add passport

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 
@@ -10,6 +12,38 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        });
+    })
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', directoryRouter);
 
