5201479bcd1cff99c777e258e6cff345c546e4a3
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-16 18:33:34 -0800

    add route for post_user_form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,25 @@
-
+const mongoose = require('mongoose');
+const User = require('../models/user');
 exports.get_user_form = (req, res) => {
-    res.send('get user form');
+    res.render('./users/user_form');
 }
-
 exports.post_user_form = (req, res) => {
-    res.send('post user form');
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        password: req.body.password,
+        membershipStatus: req.body.membershipStatus
+    })
+
+    newUser.save();
+
+    res.redirect('/user_login');
+    
 }
 
 exports.get_user_login = (req, res) => {
-    res.send('get user login');
+    res.render('./users/user_login')
 }
 
 exports.post_user_login = (req, res) => {
