65d4f0b12c0208303fcad3da5bff83baae67d7d7
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-17 11:37:53 -0800

    fix issue with req.body names

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,11 +5,11 @@ exports.get_user_form = (req, res) => {
 }
 exports.post_user_form = (req, res) => {
     const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        userName: req.body.userName,
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        userName: req.body.username,
         password: req.body.password,
-        membershipStatus: req.body.membershipStatus
+        membershipStatus: req.body.membership
     })
 
     newUser.save();
@@ -19,7 +19,7 @@ exports.post_user_form = (req, res) => {
 }
 
 exports.get_user_login = (req, res) => {
-    res.render('./users/user_login')
+    res.render('./users/user_login',  { user: req.username });
 }
 
 exports.post_user_login = (req, res) => {
