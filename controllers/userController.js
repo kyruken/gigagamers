01ef164e0222d10b97183b0a8e845021cc010df2
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-18 11:38:30 -0800

    add user validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const {body, validationResults} = require('express-validator');
 const User = require('../models/user');
 exports.get_user_form = (req, res) => {
     res.render('./users/user_form');
@@ -11,6 +12,24 @@ exports.post_user_form = (req, res) => {
         password: req.body.password,
         membershipStatus: req.body.membership
     })
+
+    body("firstname", "First name must not be empty")
+    .trim()
+    .isLength({min: 1, max: 50})
+    .escape()
+    body("lastname", "Last name must not be empty")
+    .trim()
+    .isLength({min: 1, max: 50})
+    .escape()
+    body("username", "Username must not be empty")
+    .trim()
+    .isLength({min: 1, max:20})
+    .escape()
+    body("password", "Password must not be empty")
+    .trim()
+    .isLength({min: 1, max: 16})
+    .escape()
+
 
     newUser.save();
 
