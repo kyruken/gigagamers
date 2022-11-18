f64d368e14e7026346054c3b79d4bcbcdae1fce6
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-18 13:03:22 -0800

    add bcryptjs hash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 const {body, validationResults} = require('express-validator');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 exports.get_user_form = (req, res) => {
     res.render('./users/user_form');
 }
-exports.post_user_form = (req, res) => {
-    const newUser = new User({
-        firstName: req.body.firstname,
-        lastName: req.body.lastname,
-        userName: req.body.username,
-        password: req.body.password,
-        membershipStatus: req.body.membership
-    })
+exports.post_user_form = (req, res, next) => {
 
     body("firstname", "First name must not be empty")
     .trim()
@@ -30,15 +24,28 @@ exports.post_user_form = (req, res) => {
     .isLength({min: 1, max: 16})
     .escape()
 
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+
+        const newUser = new User({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            password: hashedPassword,
+            membershipstatus: req.body.membership
+        })
 
     newUser.save();
 
-    res.redirect('/user_login');
+    res.redirect('/');
+    })
     
 }
 
 exports.get_user_login = (req, res) => {
-    res.render('./users/user_login',  { user: req.username });
+    //Passport sends messages using req.session.messages, but you need to
+    //set failureMessage: true in passport.authenticate
+    res.render('./users/user_login',  { user: req.username, message: req.session.messages});
 }
 
 exports.post_user_login = (req, res) => {
