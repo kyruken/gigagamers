bd6dcf9102d38193d8c49510c1c2e901704d94f0
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-18 13:02:57 -0800

    put passport authentication, add fail message

--- routes/directory.js
+++ routes/directory.js
@@ -1,9 +1,69 @@
 const express = require('express');
-
+const session = require('express-session');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
 const router = express.Router();
 
 const userController = require('../controllers/userController');
 const messageController = require('../controllers/messageController');
+
+
+const User = require('../models/user');
+
+router.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+//We use passReqToCallback to get a req object into our callback so that we can
+//edit the req.session.messages back to an empty array.
+// otherwise: ["incorrect username", "incorrect username", etc.]
+passport.use(
+    new LocalStrategy({passReqToCallback: true}, (req, username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            req.session.messages = [];
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        });
+    })
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/log-in",
+      failureMessage: true
+    })
+  );
+
+router.get("/log-out", (req, res, next) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+
 
 router.get('/', (req, res) => {
     res.render("homepage", {user: req.user});
