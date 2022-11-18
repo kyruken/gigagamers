8cf2b42aa3b0c48958fb57949eb79ca0115404d5
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-18 13:03:32 -0800

    remove passport authentication

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const session = require('express-session');
-const expressLayouts = require('express-ejs-layouts')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const app = express();
 
-const User = require('./models/user');
+const userController = require('./controllers/userController');
 
 const mongoDb = "mongodb+srv://kairuken:swagman@cluster0.2cnpqko.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(mongoDb, {useUnifiedTopology: true, useNewUrlParser: true})
@@ -20,57 +18,11 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
-
-passport.use(
-    new LocalStrategy((username, password, done) => {
-        User.findOne({ username: username }, (err, user) => {
-            if (err) {
-                return done(err);
-            }
-            if (!user) {
-                return done(null, false, { message: "Incorrect username" });
-            }
-            if (user.password !== password) {
-                return done(null, false, { message: "Incorrect password" });
-            }
-            return done(null, user);
-        });
-    })
-);
-
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
-    });
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
 
-app.post(
-    "/",
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/"
-    })
-  );
-
-app.get("/log-out", (req, res, next) => {
-    req.logout(function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-  });
 
 app.use('/', directoryRouter);
+
 
 app.listen(process.env.PORT || 3000);
 
