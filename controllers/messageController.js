62de562cae4df8b98b3f3037e0c43ad074325877
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-19 10:58:19 -0800

    create logic for post_message_form, add routes

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,7 +1,39 @@
+const mongoose = require('mongoose');
+const Message = require('../models/message')
+const User = require('../models/user')
+
 exports.get_message_form = (req, res) => {
-    res.send('get message form');
+    res.render('./messages/message_form');
 }
 
-exports.post_message_form = (req, res) => {
-    res.send('post message form');
+exports.post_message_form = (req, res, next) => {
+
+    const newMessage = new Message({
+        title: req.body.title,
+        body: req.body.body,
+        author: req.body.author,
+        date: new Date()
+    })
+
+
+    newMessage.save();
+
+    User.findByIdAndUpdate(req.body.authorid, {message: newMessage}, (err, updatedUser) => {
+        if(err) {
+            return next(err);
+        }
+
+        res.redirect(newMessage.url);
+        
+    })
+
+}
+
+exports.get_message_detail = (req, res) => {
+    res.send("get message page");
+
+}
+
+exports.post_message_detail = (req, res) => {
+    
 }
