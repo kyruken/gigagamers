8411f02b1ade926d338955c997fbfd41ac26ff77
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-21 11:46:54 -0800

    add sanitation and validation to user_form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,52 +1,63 @@
 const mongoose = require('mongoose');
-const {body, check, validationResult} = require('express-validator');
+const {body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+
 exports.get_user_form = (req, res) => {
     res.render('./users/user_form');
 }
-exports.post_user_form = (req, res, next) => {
+
+exports.post_user_form = [
 
     body("firstname", "First name must not be empty")
     .trim()
     .isLength({min: 1, max: 50})
-    .escape()
+    .escape(),
     body("lastname", "Last name must not be empty")
     .trim()
     .isLength({min: 1, max: 50})
-    .escape()
+    .escape(),
     body("username", "Username must not be empty")
     .trim()
     .isLength({min: 1, max:20})
-    .escape()
+    .escape(),
     body("password", "Password must not be empty")
     .trim()
     .isLength({min: 1, max: 16})
-    .escape()
+    .escape(),
 
-    const errors = validationResult(req);
-
-    console.log(errors);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()})
-    }
-    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-        if (err) return next(err);
-
-        const newUser = new User({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            username: req.body.username,
-            password: hashedPassword,
-            membershipstatus: req.body.membership
-        })
-
-    newUser.save();
-
-    res.redirect('/');
-    })
+    (req, res, next) => {
+        const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+            // Build your resulting errors however you want! String, object, whatever - it works!
+            return `* ${msg} *`;
+          };
     
-}
+        const errors = validationResult(req).formatWith(errorFormatter);
+        
+        if (!errors.isEmpty()) {
+            console.log("swaggyialsdlsad");
+            res.render('./users/user_form', {error: errors.array()});
+            return;
+        }
+    
+        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+            if (err) return next(err);
+    
+            const newUser = new User({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                username: req.body.username,
+                password: hashedPassword,
+                membershipstatus: req.body.membership
+            })
+    
+        newUser.save();
+    
+        res.redirect('/');
+        })
+        
+    }
+]
 
 exports.get_user_login = (req, res) => {
     //Passport sends messages using req.session.messages, but you need to
